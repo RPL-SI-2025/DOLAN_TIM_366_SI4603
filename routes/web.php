e5718cd290b5f8cf98c5382fc7ef3d62577f1039
adityajakap <aditya.jakap12@gmail.com>
2025-04-10 10:38:00 +0700

    Layout Dashboad

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,23 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('user.welcome');
+});
+
+Route::prefix('dashboard')->name('dashboard.')->group(function () {
+    // Dashboard
+    Route::get('', function () {
+        return view('dashboard.dashboard');
+    })->name('index');
+
+    Route::get('itineraries', [ItineraryController::class, 'index'])->name('itineraries.index')->middleware('auth');
+    
+    // User Admin
+    Route::get('admin/download', [AdminController::class, 'download_pdf'])->name('admin.download_pdf');
+    Route::resource('admin', AdminController::class);
+
+
+    // Gallery Admin
+    Route::get('gallery/download', [GalleryController::class, 'download_pdf'])->name('gallery.download_pdf');
+    Route::resource('gallery', GalleryController::class);
 });
