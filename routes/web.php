c44208e2473fdc3bace351ff7d3c393cb07e3e65
alfiratria <127504688+alfiratria@users.noreply.github.com>
2025-04-24 18:22:11 +0700

    homecontroller

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\DestinationController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\HomeController;
 // use App\Http\Controllers\GalleryController;
 
 // Halaman register
