992e6c6f7ed37c46847703a8284d7e4486d09015
alfiratria <127504688+alfiratria@users.noreply.github.com>
2025-06-01 01:32:20 +0700

    Route Merch

--- routes/web.php
+++ routes/web.php
@@ -69,6 +69,14 @@ Route::prefix('dashboard')->name('dashboard.')->group(function () {
     Route::get('articles/{article}/edit', [ArticleController::class, 'edit'])->name('articles.edit')->middleware(['auth', 'role:admin,super_admin']);
     Route::delete('articles/{article}', [ArticleController::class, 'destroy'])->name('articles.destroy')->middleware(['auth', 'role:admin,super_admin']);
 
+      // Merchandise
+    Route::get('merchandise', [MerchandiseController::class, 'index'])->name('merchandise.index')->middleware(['auth', 'role:admin,super_admin']);
+    Route::get('merchandise/create', [MerchandiseController::class, 'create'])->name('merchandise.create')->middleware(['auth', 'role:admin,super_admin']);
+    Route::post('merchandise', [MerchandiseController::class, 'store'])->name('merchandise.store')->middleware(['auth', 'role:admin,super_admin']);
+    Route::get('merchandise/{merchandise}/edit', [MerchandiseController::class, 'edit'])->name('merchandise.edit')->middleware(['auth', 'role:admin,super_admin']);
+    Route::put('merchandise/{merchandise}', [MerchandiseController::class, 'update'])->name('merchandise.update')->middleware(['auth', 'role:admin,super_admin']);
+    Route::delete('merchandise/{merchandise}', [MerchandiseController::class, 'destroy'])->name('merchandise.destroy')->middleware(['auth', 'role:admin,super_admin']);
+
     // Profile
     Route::prefix('user/profile')->name('user.profile.')->middleware('auth')->group(function () {
         Route::get('/', [ProfileController::class, 'show'])->name('show');
