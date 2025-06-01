15d95f2e7043cb9dd4778e843d22024cbda2c769
adityajakap <aditya.jakap12@gmail.com>
2025-06-01 18:03:52 +0700

    fix merch and admin dashboard

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -53,6 +53,7 @@
             <span class="flex-1 ms-3 whitespace-nowrap">Article</span>
           </a>
           </li>
+                      @if (Auth::check() && (Auth::user()->isSuperAdmin()))
           <li>
           <a href="/dashboard/admin"
          class="flex items-center p-2 {{ Request::is('dashboard/admin') ? 'active text-white bg-green-600' : 'text-gray-900 hover:bg-gray-100' }} rounded-lg group">
@@ -62,10 +63,10 @@
             <path
               d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
             </svg>
-            @if (Auth::check() && (Auth::user()->isSuperAdmin()))
             <span class="flex-1 ms-3 whitespace-nowrap">Admin</span>
           </a>
           </li>
+          @endif
           <li>
           <a href="{{ route('dashboard.merchandise.index') }}"
             class="flex items-center p-2 {{ Request::is('dashboard/merchandise') ? 'active text-white bg-green-600' : 'text-gray-900 hover:bg-gray-100' }} rounded-lg group">
@@ -74,26 +75,10 @@
             aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
             <path d="M16 6a2 2 0 0 0-2-2h-1V3a3 3 0 1 0-6 0v1H6a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V6Zm-8-3a1 1 0 1 1 2 0v1H8V3Zm6 13H6V6h8v10Z"/>
             </svg>
-            @endif
+     
             <span class="flex-1 ms-3 whitespace-nowrap">Merchandise</span>
           </a>
           </li>
-          <li>
-          <a href="#"
-            class="flex items-center p-2 text-gray-900 hover:bg-gray-100 rounded-lg group">
-            <svg
-            class="flex-shrink-0 w-5 h-5 text-gray-500 group-hover:text-gray-900 transition duration-75"
-            aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
-            viewBox="0 0 24 24">
-            <path fill-rule="evenodd" d="M13 10a1 1 0 0 1 1-1h.01a1 1 0 1 1 0 2H14a1 1 0 0 1-1-1Z"
-              clip-rule="evenodd" />
-            <path fill-rule="evenodd"
-              d="M2 6a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v12c0 .556-.227 1.06-.593 1.422A.999.999 0 0 1 20.5 20H4a2.002 2.002 0 0 1-2-2V6Zm6.892 12 3.833-5.356-3.99-4.322a1 1 0 0 0-1.549.097L4 12.879V6h16v9.95l-3.257-3.619a1 1 0 0 0-1.557.088L11.2 18H8.892Z"
-              clip-rule="evenodd" />
-            </svg>
-            <span class="flex-1 ms-3 whitespace-nowrap">Gallery <em>(coming soon)</em></span>
-        </a>
-      </li>
     </ul>
   </div>
 </aside>
